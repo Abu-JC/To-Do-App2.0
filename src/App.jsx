a67a6a18fc8ee0881e5c559a3864dc8416bdf314
Abu-JC <brianmayabi24@gmail.com>
2026-06-27 23:45:05 +0300

    Testing List Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import TasksList from "./TasksList.jsx"
 function App(){
-  const tasks = [{id=0,text='Go Buy A cake',completed=false},
-           {id=1,text='Walk to church',completed=true},
-           {id=2,text='Make the React App',completed=true},
-           {id=3,text='Learn MongoDB',completed=false},      
+  const tasks = [{id:0,text:'Go Buy A cake',completed:false},
+           {id:1,text:'Walk to church',completed:true},
+           {id:2,text:'Make the React App',completed:true},
+           {id:3,text:'Learn MongoDB',completed:false},      
   ]
   return(
     <>
-    
+    <TasksList tasks={tasks}/>
     </>
   )
 }
--- src/TasksList.jsx
+++ src/TasksList.jsx
@@ -1,5 +1,13 @@
-import App from "./App.jsx"
-function TasksList(){
-
+function TasksList({tasks}){
+    return(
+        <div>
+            <ul>
+                {tasks.map(task=>{
+                   return <li key={task.id}>{task.text}</li>
+                })}
+            </ul>
+        </div>
+        
+    )
 }
 export default TasksList
